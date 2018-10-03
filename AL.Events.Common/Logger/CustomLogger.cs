7ed252cbfd3f255507728d958a57a55b99efed81
laskalexjob <laskalexjob@gmail.com>
2018-10-03 17:05:51 +0300

    Ev-1.1: log4net added with local file

---
+++ AL.Events.Common/Logger/CustomLogger.cs
@@ -0,0 +1,42 @@
+﻿using log4net;
+using log4net.Config;
+using System;
+using System.Configuration;
+using System.IO;
+using System.Reflection;
+
+namespace AL.Events.Common.Logger
+{
+    public class CustomLogger : ICustomLogger
+    {
+        private const string _loggerName = "CustomLogger";
+        private readonly ILog _log;
+
+        public CustomLogger()
+        {
+            var config = GetConfigFile("Log.config");
+
+            if(config.Exists)
+            {
+                XmlConfigurator.Configure(config);
+                _log = LogManager.GetLogger(_loggerName);
+            }
+        }
+
+        private FileInfo GetConfigFile(string configName)
+        {
+            var currentDirectory = Path.GetDirectoryName(new Uri(Assembly.GetExecutingAssembly().CodeBase).LocalPath);
+
+            var fullPath = string.Format($"{currentDirectory}\\{configName}");
+
+            var fileInfo = new FileInfo(fullPath);
+
+            return fileInfo;
+        }
+
+        public void WriteToLogInfo(string info)
+        {
+            _log.Info(info);
+        }
+    }
+}
